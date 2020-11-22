ae3813707a5986f3e718345a62b037ed7eb437fc
Sai Sasank <sai.sasank.yadati@gmail.com>
2020-11-22 11:24:09 +0530

    synchronous update

--- DPAgent.py
+++ DPAgent.py
@@ -1,3 +1,4 @@
+import copy
 class DPAgent():
     def __init__(self, mdp, discount_rate=1):
         self.mdp = mdp
@@ -9,19 +10,23 @@ class DPAgent():
         self.discount_rate = discount_rate
 
     def evaluate_policy(self):
+        values = [0]*self.mdp.num_states
         for s in range(self.mdp.num_states):
-            self.evaluate_policy_for_state(s)
-            self.value_fn[s] = round(self.value_fn[s], 3)
+            values[s] = self.evaluate_policy_for_state(s)
+            values[s] = round(values[s], 3)
+        self.value_fn = copy.deepcopy(values)
     
     def evaluate_policy_for_state(self, s):
         assert s < self.mdp.num_states
+        value_s = 0
         for a in range(self.mdp.num_actions):
             for transition in self.mdp.P[s][a]:
                 # transition informs the next state, probability of it, reward and if state is terminal
                 pr_s_ = transition[0] # prob of next state s_ given s and a
                 s_ = transition[1] # value of next state s_ given s and a
                 r_s_ = transition[2] # reward of next state s_ given s and a
-                self.value_fn[s] += self.policy[s][a] * pr_s_ * (r_s_ + self.discount_rate * self.value_fn[s_])
+                value_s += self.policy[s][a] * pr_s_ * (r_s_ + self.discount_rate * self.value_fn[s_])
+        return value_s
 
     def policy_improvement():
         pass
